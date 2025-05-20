f61276e9e8d7aa65cd583b49c3403081e09b883e
navi-qq <201188602+navi-qq@users.noreply.github.com>
2025-05-20 19:17:18 +0800

    Set window state to maximize

--- library/Program.cs
+++ library/Program.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace library
 {
     internal static class Program
@@ -11,7 +13,11 @@ namespace library
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Form1 form = new Form1();
+
+            form.WindowState = FormWindowState.Maximized;
+          
+            Application.Run(form);
         }
     }
 }
