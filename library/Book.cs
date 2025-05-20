7cabc11bcddd4616956c8608285ed0d5b782bd7b
navi-qq <201188602+navi-qq@users.noreply.github.com>
2025-05-20 19:11:09 +0800

    Add Book class

---
+++ library/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class Book
+    {
+
+        public String title;
+        public String author;
+        public int numberOfPages;
+        public bool readingStatus;
+
+
+        public Book(string title, string author, int numberOfPages, bool readingStatus)
+        {
+            this.title = title;
+            this.author = author;
+            this.numberOfPages = numberOfPages;
+            this.readingStatus = readingStatus;
+        }
+
+        public void setReadingStatus(bool newReadigStatus)
+        {
+            this.readingStatus = newReadigStatus;
+        }
+
+        public void edit()
+        {
+
+        }
+    }
+}
