bd0fb5c23e43c6d1b9d03583e2096842d6a563b4
navi-qq <201188602+navi-qq@users.noreply.github.com>
2025-05-21 22:13:04 +0800

    Add Book Data Grid View

---
+++ library/BookDataGridView.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace library
+{
+    partial class BookDataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            bookDataGrid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)bookDataGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // bookDataGrid
+            // 
+            bookDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            bookDataGrid.Dock = DockStyle.Fill;
+            bookDataGrid.Location = new Point(0, 0);
+            bookDataGrid.Name = "bookDataGrid";
+            bookDataGrid.RowHeadersWidth = 51;
+            bookDataGrid.Size = new Size(836, 469);
+            bookDataGrid.TabIndex = 0;
+            // 
+            // BookDataGridView
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(836, 469);
+            Controls.Add(bookDataGrid);
+            Name = "BookDataGridView";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "BookDataGridView";
+            FormClosed += BookDataGridView_FormClosed;
+            Load += BookDataGridView_Load;
+            ((System.ComponentModel.ISupportInitialize)bookDataGrid).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView bookDataGrid;
+    }
+}
---
+++ library/BookDataGridView.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace library
+{
+    public partial class BookDataGridView : Form
+    {
+
+        List<Book> library = new List<Book>();
+        String[] tempData = new string[4];
+        DataTable dataTable = new DataTable();
+        public BookDataGridView()
+        {
+            InitializeComponent();
+
+            //this.Clos
+        }
+
+        public void setData(List<Book> library)
+        {
+            this.library = library;
+        }
+
+        private void BookDataGridView_Load(object sender, EventArgs e)
+        {
+            dataTable.Columns.Add("Title");
+            dataTable.Columns.Add("Author");
+            dataTable.Columns.Add("Number Of Pages");
+            dataTable.Columns.Add("Reading Status");
+
+            foreach (var book in library)
+            {
+
+                tempData[0] = book.title;
+                tempData[1] = book.author;
+                tempData[2] = book.numberOfPages.ToString();
+
+                if (book.readingStatus)
+                {
+                    tempData[3] = "Completed";
+                }
+                else tempData[3] = "On Read";
+
+
+                dataTable.Rows.Add(tempData);
+            }
+
+            bookDataGrid.DataSource = dataTable;
+        }
+
+        private void BookDataGridView_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           
+        }
+    }
+
+
+}
